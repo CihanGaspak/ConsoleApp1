75cc351851df84d1a765c4fa010376903fc3cd6f
CihanGaspak <cihangir2344@gmail.com>
2023-11-20 21:36:37 +0300

    Genel Konular, oparatör,değiişken, sorgular.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Hello,");
-Console.Write("World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Hello, ");
+        Console.WriteLine("World!");
+        
+        float floatSayi = 12.1f;
+        int intSayi = 124;
+        Console.WriteLine(floatSayi);
+        Console.WriteLine(intSayi); //124
+
+        Console.WriteLine(intSayi++); //124
+        Console.WriteLine(++intSayi); //126
+        Console.WriteLine(5 < 1); //false
+        Console.ReadKey();
+    }
+}
